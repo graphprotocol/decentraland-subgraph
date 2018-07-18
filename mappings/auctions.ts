a89b2b78329d294275e874567acf796f99f2c6ba
Arseniy Klempner <adklempner@gmail.com>
2018-07-18 10:48:30 -0500

    Creates relationship between Parcels and Auctions

--- mappings/auctions.ts
+++ mappings/auctions.ts
@@ -2,25 +2,31 @@ export function handleAuctionCreated(event: AuctionCreated): void {
   let auction = new Entity()
   let id = event.id.toHex()
 
-  auction.setString('asset_id', event.assetId.toHex())
-  auction.setAddress('owner', event.seller)
+  auction.setString('id', id)
+  auction.setAddress('seller', event.seller)
+  auction.setString('for_parcel', event.assetId.toHex())
   auction.setU256('price', event.priceInWei)
   auction.setU256('expires_at', event.expiresAt)
   auction.setString('status', 'open')
   auction.setBoolean('is_sold', false)
   auction.setAddress('marketplace_id', event.address)
-  //TODO: need to get block number from blockhash
-  // auction.setU256('block_number', block_number);
-  //TODO: check if asset is a parcel or estate
-  auction.setString('type', 'parcel')
+
+  let parcel = new Entity()
+  parcel.setString('active_auction', id)
 
   database.create('Auction', id, auction)
+  database.update('Parcel', event.assetId.toHex(), parcel)
 }
 
 export function handleAuctionCancelled(event: AuctionCancelled): void {
   let auction = new Entity()
   auction.setString('status', 'cancelled')
+
+  let parcel = new Entity()
+  parcel.setString('active_auction', null)
+
   database.update('Auction', event.id.toHex(), auction)
+  database.update('Parcel', event.assetId.toHex(), parcel)
 }
 
 export function handleAuctionSuccessful(event: AuctionSuccessful): void {
@@ -29,5 +35,9 @@ export function handleAuctionSuccessful(event: AuctionSuccessful): void {
   auction.setAddress('buyer', event.winner)
   auction.setU256('price', event.totalPrice)
 
+  let parcel = new Entity()
+  parcel.setString('active_auction', null)
+
   database.update('Auction', event.id.toHex(), auction)
+  database.update('Parcel', event.assetId.toHex(), parcel)
 }
--- mappings/parcels.ts
+++ mappings/parcels.ts
@@ -1,8 +1,10 @@
 export function handleLandTransfer(event: Transfer): void {
   let parcel = new Entity()
   let id = event.assetId.toHex()
+  let registry = LANDRegistry.bind(event.address, event.blockHash)
+  parcel.setString('id', id)
   parcel.setAddress('owner', event.to)
-  database.update('Parcel', id, parcel)
+  database.create('Parcel', id, parcel)
 }
 
 export function handleLandUpdate(event: Update): void {

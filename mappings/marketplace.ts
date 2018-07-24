786410684d3d24f305848ca059fac1e8321a4217
Jannis Pohlmann <contact@jannispohlmann.de>
2018-07-25 00:32:13 +0200

    WIP: Improve data source

--- mappings/marketplace.ts
+++ mappings/marketplace.ts
@@ -11,13 +11,13 @@ export function handleAuctionCreated(event: AuctionCreated): void {
   auction.setString('txStatus', 'confirmed')
   // TODO: auction.setString('txHash', event.transactionHash)
   auction.setAddress('seller', event.seller)
-  auction.set('buyer', Value.fromNull())
+  auction.unset('buyer')
   auction.setU256('price', event.priceInWei)
   auction.setU256('expiresAt', event.expiresAt)
   auction.setAddress('contract', event.id)
   // TODO: auction.setU256('blockNumber', event.blockNumber)
   // TODO: auction.setU256('blockTimeCreatedAt', event.blockTime)
-  // TODO: auction.setAddress('marketplace', ...)
+  auction.setAddress('marketplace', event.address)
   // This is the contract address of the marketplace contract; we
   // should probably make that available here
   database.create('Auction', auctionId, auction)
@@ -26,6 +26,8 @@ export function handleAuctionCreated(event: AuctionCreated): void {
   let parcel = new Entity()
   parcel.setString('id', parcelId)
   parcel.setString('activeAuction', auctionId)
+  parcel.setAddress('auctionOwner', event.seller)
+  parcel.setU256('auctionPrice', event.priceInWei)
   database.update('Parcel', parcelId, parcel)
 }
 
@@ -36,6 +38,7 @@ export function handleAuctionCancelled(event: AuctionCancelled): void {
   // Mark the auction as cancelled
   let auction = new Entity()
   auction.setString('id', auctionId)
+  auction.setString('type', 'parcel')
   auction.setString('status', 'cancelled')
   // TODO: auction.setU256('blockTimeUpdatedAt', event.blockTime)
   database.update('Auction', auctionId, auction)
@@ -44,6 +47,8 @@ export function handleAuctionCancelled(event: AuctionCancelled): void {
   let parcel = new Entity()
   parcel.setString('id', parcelId)
   parcel.set('activeAuction', Value.fromNull())
+  parcel.set('auctionOwner', Value.fromNull())
+  parcel.set('auctionPrice', Value.fromNull())
   database.update('Parcel', parcelId, parcel)
 }
 
@@ -54,6 +59,7 @@ export function handleAuctionSuccessful(event: AuctionSuccessful): void {
   // Mark the auction as sold
   let auction = new Entity()
   auction.setString('id', auctionId)
+  auction.setString('type', 'parcel')
   auction.setString('status', 'sold')
   auction.setAddress('buyer', event.winner)
   auction.setU256('price', event.totalPrice)
@@ -65,5 +71,7 @@ export function handleAuctionSuccessful(event: AuctionSuccessful): void {
   parcel.setString('id', parcelId)
   parcel.setAddress('owner', event.winner)
   parcel.set('activeAuction', Value.fromNull())
+  parcel.set('auctionOwner', Value.fromNull())
+  parcel.set('auctionPrice', Value.fromNull())
   database.update('Parcel', parcelId, parcel)
 }
